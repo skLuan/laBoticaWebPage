f8f1e31249c75ba6a219da99732c843e505bdc43
Luan E <luan.erazo@skyrocketlab.space>
2023-12-08 17:49:09 -0500

    config files, tailwind installed and a proper setup

---
+++ wp-content/themes/rq_theme/footer.php
@@ -0,0 +1,17 @@
+<footer class="flex flex-col justify-around py-16 text-white lg:flex-row m-ext bg-green-dark">
+    <ul>
+
+    </ul>
+    <ul class="mt-12 lg:ml-5 lg:mt-0">
+    </ul>
+    <ul class="mt-12 lg:ml-5 lg:mt-0">
+
+    </ul>
+    <ul class="mt-12 lg:mt-0">
+    </ul>
+</footer>
+<div class="flex flex-row my-2 text-gray-500 bg-white m-ext "><span class="mx-auto">copyright 2024</span></div>
+<?php wp_footer() ?>
+</body>
+
+</html>
