fc74ee99174d472631080442683fccd2fd19c374
Luan E <luan.erazo@skyrocketlab.space>
2023-12-28 13:27:08 -0500

    changed the theme name

---
+++ wp-content/themes/labotica_theme/footer.php
@@ -0,0 +1,17 @@
+<footer class="bg-rqPurple text-rqWhite-Silver flex flex-col px-2 items-center">
+    <ul>
+
+    </ul>
+    <ul class="mt-12 lg:ml-5 lg:mt-0">
+    </ul>
+    <ul class="mt-12 lg:ml-5 lg:mt-0">
+
+    </ul>
+    <ul class="mt-12 lg:mt-0">
+    </ul>
+    <div class=""><span class="mx-auto">copyright 2024</span></div>
+</footer>
+<?php wp_footer() ?>
+</body>
+
+</html>
---
+++ wp-content/themes/labotica_theme/front-page.php
@@ -0,0 +1,32 @@
+<?php get_header() ?>
+
+
+<section id="">
+  
+</section>
+
+<section id="" class="mt-4">
+  
+    </div>
+
+</section>
+<section id="">
+
+</section>
+<section id="">
+
+</section>
+<section id="">
+
+</section>
+<section id="">
+
+</section>
+<?php
+// while (have_posts()) {
+//     the_post();
+//     the_content();
+// }
+?>
+
+<?php get_footer() ?>
