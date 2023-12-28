47b39fba5fb4831f8cc81f10081b97e397540de5
skLuan <64113803+skLuan@users.noreply.github.com>
2023-12-28 11:04:02 -0500

    Delete important info

--- db/local_backup.sh
+++ db/local_backup.sh
@@ -4,7 +4,7 @@
 
 # Update lines 7-8 with your database name, user name, and password
 echo 'Updating our MySQL database...';
-mysql --user='u904348789_trbKy' --password='zxvdfimRBw' u904348789_kBmaU < ./local_data.sql
+mysql --user='name' --password='pass' serverDataBase < ./local_data.sql
 # mysql --user='admin' --password='admin' wp_biotienda < ./update.sql
 
 echo 'Update complete!';
